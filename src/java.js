a8dd1ecaa0093fbe62b63f401be07f3957de4e3c
pletschthai <146131783+pletschthai@users.noreply.github.com>
2023-11-16 15:00:25 -0700

    Working on the information sections

--- src/java.js
+++
@@ -1,6 +0,0 @@
-// Get the number of slides
-const slides = document.querySelectorAll(".slide-link");
-const numSlides = slides.length;
-
-// Set the CSS custom property '--num-slides' to the number of slides
-document.documentElement.style.setProperty("--num-slides", numSlides);
